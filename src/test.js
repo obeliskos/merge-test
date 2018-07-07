cf5159f83cf6fa38f3b16ec83a465e79fe640245
obeliskos <github@obeliskos.com>
2018-07-07 05:35:24 -0400

    b-chk2

--- src/test.js
+++ src/test.js
@@ -1,3 +1,6 @@
+/**
+ * Some jsdoc info for test1
+ */
 function test1() {
    for(let x = 1; x <= 100; x++) {
       console.log("x: " + x);
@@ -12,14 +15,17 @@ function test3() {
 
 }
 
+/**
+ * Some jsdoc info for test10
+ */
 function test10() {
 
 }
 
 function test11() {
-
+   throw new Error("Not implementted");
 }
 
 function test12() {
-   
+   throw new Error("Not implementted");
 }
