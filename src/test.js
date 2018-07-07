0997953d161ecac3eef927026702fb76d418709f
obeliskos <github@obeliskos.com>
2018-07-07 05:44:51 -0400

    m-chk3

--- src/test.js
+++ src/test.js
@@ -1,25 +1,53 @@
+/**
+ * My test1 func
+ */
 function test1() {
    for(let x = 1; x <= 100; x++) {
       console.log("x: " + x);
    }
 }
 
+/**
+ * My test2 func
+ * @param {*} a 
+ * @param {*} b 
+ */
 function test2(a, b) {
    return a * b;
 }
 
-function test3() {
-
+/**
+ * My test3 func
+ * @param {*} a 
+ * @param {*} b 
+ */
+function test3(a, b) {
+   return a / b;
 }
 
-function test4() {
-
+/**
+ * My test4 func
+ * @param {*} a 
+ * @param {*} b 
+ */
+function test4(a, b) {
+   return a + b;
 }
 
-function test5() {
-
+/**
+ * My test5 func
+ * @param {*} a 
+ * @param {*} b 
+ */
+function test5(a, b) {
+   return a - b;
 }
 
-function test6() {
-   
+/**
+ * My test6 func
+ * @param {*} a 
+ * @param {*} b 
+ */
+function test6(a, b) {
+   return a % b;
 }
